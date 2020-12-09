b539f9d291d5e1061cdd2c37443f1e2b547f3958
manan-crowdbotics <61427302+manan-crowdbotics@users.noreply.github.com>
2020-12-09 15:35:44 +0530

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -2,6 +2,8 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 
+import CbSplashScreen1183498Navigator from '../features/CbSplashScreen1183498/navigator';
+import HomeScreen2183497Navigator from '../features/HomeScreen2183497/navigator';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
@@ -13,6 +15,18 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+    
+    CbSplashScreen1183498: {
+      screen: CbSplashScreen1183498
+    }
+      screen: CbSplashScreen1183498Navigator
+    },
+
+    HomeScreen2183497: {
+      screen: HomeScreen2183497
+    }
+      screen: HomeScreen2183497Navigator
+    },
 
     /** new navigators can be added here */
     SplashScreen: {
